586d2d9ba87dfb7fe2629e3f1a5475ed1c177c39
Jason Lin <Jasonlin1079@gmail.com>
2026-01-14 18:24:12 -0500

    Made enter button wider and added click me button for testing

--- projects/calculator.js
+++ projects/calculator.js
@@ -1,3 +1,30 @@
+
+const keypad = document.getElementById('calculator-keys');
+const screen = document.getElementById('calc-screen');
+
+keypad.addEventListener('click', (event) => {
+
+    const target = event.target; // basically we set the sort of element to a const target
+
+    if (!target.matches('button')) {
+        return;
+    }
+
+    const value = target.getAttribute('data-val');
+
+    screen.value += value;
+    // So we set the target to a constant whatever this is. 
+
+});
+
+const demo = document.getElementById('demo');
+
+document.addEventListener('click', myfunction1);
+
+function myfunction1() {
+    document.getElementById('demo').innerHTML += "Hey guys ";
+}
+
 console.log("hello");
 
 let history = [];
