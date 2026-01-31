456295bba629c13eb5ffb92b9e21ed359e89637d
Jason Lin <Jasonlin1079@gmail.com>
2026-01-30 20:43:09 -0500

    test changes

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-document.addEventListener('DOMContentLoaded', () => {
 
-    
+class TechStackVisualizer {
+    constructor(data, containerId) {
+        this.data = data;
+        this.container = document.getElementById(containerId);
+        this.grid = document.getElementById('stack-grid');
+        this.details = document.getElementById('stack-details');
+        this.filters = document.querySelectorAll('.stack-filters button0');
+
+        this.init();
+    }
+
+    init() {
+        this.render('all');
+        this.attachEvents();
+    }
 }
+document.addEventListener('DOMContentLoaded', () => {
+    new TechStackVisualizer(techStackData, 'tech-stack');
+});
